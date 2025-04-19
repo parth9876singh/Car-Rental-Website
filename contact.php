d89d496f21599c4cd4f7f28070a781a4ea69e4fa
Parth Singh <147314413+parth9876singh@users.noreply.github.com>
2025-04-19 17:06:31 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -19,8 +19,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'singhparth2318.com@gmail.com';
-        $mail->Password   = 'swcs nirz aqfl lojq';
+        $mail->Username   = '';
+        $mail->Password   = '';
         $mail->SMTPSecure = 'tls';
         $mail->Port       = 587;
 
